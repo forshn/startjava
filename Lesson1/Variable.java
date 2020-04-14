292e9782199935d9babd25838aa5f7f77835444e
Nikolay <forsh_nikolay@mail.ru>
2020-04-14 22:57:08 +0300

    implement homework1

---
+++ Lesson1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+  public static void main (String[] args) {
+    byte ram = 16;
+    short sdd = 512;
+    int price = 80000;
+    long smth = 123049570;
+    float screen = 15.6f;
+    double code = 4.2908098312d;
+    char intel = 'i';
+    boolean hasWebcam = true;
+    System.out.println(ram);
+    System.out.println(sdd);
+    System.out.println(price);
+    System.out.println(smth);
+    System.out.println(screen);
+    System.out.println(code);
+    System.out.println(intel);
+    System.out.println(hasWebcam);
+
+  }
+}
