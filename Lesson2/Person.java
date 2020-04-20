5d62335d3202fd41c2f8c8769e9055d8ada98cb9
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-04-20 10:50:44 +0300

    HW for lesson 2

---
+++ Lesson2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    String sex = "мужской";
+    String name = "Вася";
+    float height = 1.93F;
+    float weight = 90.2F;
+    int age = 30;
+
+    boolean learnJava() {
+
+    }
+    String speak() {
+
+    }
+    void move() {
+
+    }
+    void run() {
+
+    }
+    void sit() {
+
+    }
+}
