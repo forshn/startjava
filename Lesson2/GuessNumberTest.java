4edcd1428394ef78fc8f9d68011b0cd6a2e67e24
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-04-23 22:15:45 +0300

    Homework

---
+++ Lesson2/GuessNumberTest.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class GuessNumberTest{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите имя первого игрока");
+        Player player1 = new Player(sc.next());
+        System.out.println("Введите имя второго игрока");
+        Player player2 = new Player(sc.next());
+
+        GuessNumber player1Num = new GuessNumber();
+        GuessNumber player2Num = new GuessNumber();
+
+
+        do {
+            System.out.println(player1.getName() + ", введите ваше число от 0 до 100:");
+            player1Num.setPlayer1Num(sc.nextInt());
+            player1Num.finalResultPlayer1();
+            if (player1Num.getPlayer1Num() == GuessNumber.compNum) {
+              break;
+            }
+
+            System.out.println(player2.getName() + ", введите ваше число от 0 до 100:");
+            player1Num.setPlayer1Num(sc.nextInt());
+            player1Num.finalResultPlayer1();
+            if (player2Num.getPlayer2Num() == GuessNumber.compNum) {
+              break;
+            }
+
+            System.out.println("Хотите продолжить, yes/no?");
+
+            String answer = sc.next();
+            if (!answer.equals("yes")) {
+                break;
+            }
+          } while (true);
+
+
+  }
+}
---
+++ Lesson2/Player.java
@@ -0,0 +1,11 @@
+public class Player{
+    private String name;
+
+    public Player(String x) {
+        name = x;
+    }
+
+    public String getName(){
+      return name;
+    }
+}
