cd81dc1ee6b717b47bbcf8bc0feb6e4f4620ae5e
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-04-23 15:11:24 +0300

    FINAL HW for 2 lesson

---
+++ Lesson2/Calculator.java
@@ -0,0 +1,51 @@
+public class Calculator {
+    private char sign = '*';
+    private int firstNumber;
+    private int secondNumber;
+
+    public void setFirstNumber(int firstNumber){
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(int secondNumber){
+        this.secondNumber = secondNumber;
+    }
+
+    public void setSign(char sign){
+        this.sign = sign;
+    }
+
+    private int result;
+    public int getResult(){
+        switch (sign) {
+        case '+':
+        result = firstNumber + secondNumber;
+        break;
+        case '-':
+        result = firstNumber - secondNumber;
+        break;
+        case '*':
+        result = firstNumber * secondNumber;
+        break;
+        case '/':
+        result = firstNumber / secondNumber;
+        break;
+        case '%':
+        result = firstNumber % secondNumber;
+        break;
+        case '^':
+        int a = 1;
+            for (int i = 0; i < secondNumber; i++) {
+                a *= firstNumber;
+            }
+            result = a;
+            break;
+        default: System.out.println("Неверно указан тип операции");
+        break;
+        }
+        return result;
+   }
+}
+
+
+
---
+++ Lesson2/CalculatorTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class CalculatorTest{
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        Scanner sc = new Scanner(System.in);
+
+        do {
+        System.out.println("Введите первое число");
+        calc.setFirstNumber(sc.nextInt());
+
+        System.out.println("Введите знак математической операции");
+        calc.setSign(sc.next().charAt(0));
+
+        System.out.println("Введите второе число");
+        calc.setSecondNumber(sc.nextInt());
+
+        System.out.println(calc.getResult());
+        System.out.println("Хотите продолжить, yes/no?");
+
+        String answer = sc.next();
+        if (!answer.equals("yes")) {
+        break;
+        }
+        }
+        while (true);
+    }
+}
